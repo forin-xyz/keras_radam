377c434cb0fa14bb8d137d0c35c3c5a1ff2e4fb6
rui.cheng <0000@whu.edu.cn>
2019-08-25 21:39:27 +0800

    use gelu in test_radam.py

--- test_radam.py
+++ test_radam.py
@@ -16,16 +16,20 @@ import numpy as np
 from radam import RAdam
 from keras.models import Sequential
 from keras import layers as L
+import keras.backend as K
+import math
+
+def gelu(x):
+    return 0.5 * x * (1 + K.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * K.pow(x, 3))))
 
 
 X = np.random.standard_normal((64 * 1000, 25))
 y = np.int64(np.sum(X * X, axis=1) > 25.)
 
-
 model = Sequential()
-model.add(L.Dense(40, input_shape=(25,), activation="relu"))
-model.add(L.Dense(64, input_shape=(40,), activation="relu"))
-model.add(L.Dense(32, input_shape=(64,), activation="relu"))
+model.add(L.Dense(40, input_shape=(25,), activation=gelu))
+model.add(L.Dense(64, input_shape=(40,), activation=gelu))
+model.add(L.Dense(32, input_shape=(64,), activation=gelu))
 model.add(L.Dropout(0.2))
 model.add(L.Dense(1, activation="sigmoid"))
 
